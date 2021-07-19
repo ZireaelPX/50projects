5d316ccdbd5e5a751e2302d1eb0b01cf5f15252f
Artem <artempavlov02@mail.ru>
2021-07-19 16:23:25 +0300

    Добавление 4 и 5 дня

---
+++ Day 5 - Blurry Loading/js/script.js
@@ -0,0 +1,22 @@
+const loadingText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+	load++;
+	if (load > 99) {
+		clearInterval(int);
+	}
+
+	loadingText.innerHTML = `${load}%`;
+	loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+function scale(number, inMin, inMax, outMin, outMax) {
+	return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
